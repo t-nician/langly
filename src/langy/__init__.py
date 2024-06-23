a75aa3747a433dd684155fe618466452f2555511
t-nician <dev@senpai.zone>
2024-06-23 13:42:19 -0400

    No rust, I lost my touch with Rust. Python for now, might mess with Lua too.

---
+++ src/langy/__init__.py
@@ -0,0 +1,6 @@
+
+
+def load_script(path: str):
+    with open(path, "r") as file:
+        script_content = file.read()
+        
---
+++ src/main.py
@@ -0,0 +1,5 @@
+import langy
+
+
+loaded_script = langy.load_script("./workspace/script.langy")
+
